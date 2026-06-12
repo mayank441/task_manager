d788413b60e4768d90ac5abb5d2616049bef2a9f
Mayank Karayat <mayankkarayat25@gmail.com>
2026-06-12 23:05:09 +0530

    fix: update CORS origin to Vercel URL

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://task-manager-zeta-coral.vercel.app'
+}));
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
